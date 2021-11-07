cb8bcb7eab68a961b88cf8e776c8863e7aa22d0f
User <User@LAB3-11>
2021-11-07 09:34:06 +0200

    Added wishlist view.

---
+++ EventyApp/EventyApp/ViewModel/WishlistsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using EventyApp.Views;
+
+namespace EventyApp.ViewModel
+{
+    class WishlistsViewModel //: INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //protected void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+        //public event Action<Page> Push;
+        //public ICommand ExploreCommand => new Command(Explore);
+        //private void Explore()
+        //{
+        //    TabControlView.;
+        //}
+    }
+}
