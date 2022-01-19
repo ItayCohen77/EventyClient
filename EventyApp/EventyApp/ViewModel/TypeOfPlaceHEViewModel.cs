2a345301dbd979597e6e96aa9ec1520bc8fd0e34
User <User@LAB3-11>
2022-01-19 13:53:49 +0200

    Added next button.

--- EventyApp/EventyApp/ViewModel/TypeOfPlaceHEViewModel.cs
+++ EventyApp/EventyApp/ViewModel/TypeOfPlaceHEViewModel.cs
@@ -91,6 +91,23 @@ namespace EventyApp.ViewModel
             }
         }
 
+        private string isvisible;
+        public string IsVisible
+        {
+            get
+            {
+                return this.isvisible;
+            }
+            set
+            {
+                if (this.isvisible != value)
+                {
+                    this.isvisible = value;
+                    OnPropertyChanged(nameof(IsVisible));
+                }
+            }
+        }
+
         public ICommand ApartmentCommand => new Command(Apartment);
         private void Apartment()
         {
@@ -99,12 +116,25 @@ namespace EventyApp.ViewModel
                 this.ChangeBorderColorApartment = "#FF5353";
                 this.Pressed = true;
             }
+            else if (this.changeBorderColorApartment == "#FF5353")
+            {
+                this.ChangeBorderColorApartment = "#B7B7B7";
+                this.ChangeBorderColorPH = "#B7B7B7";
+                this.ChangeBorderColorHB = "#B7B7B7";
+                this.Pressed = false;
+                this.IsVisible = "False";
+            }
             else
             {
                 this.ChangeBorderColorApartment = "#FF5353";
                 this.ChangeBorderColorPH = "#B7B7B7";
                 this.ChangeBorderColorHB = "#B7B7B7";
                 this.Pressed = true;
+            }
+
+            if(Pressed)
+            {
+                this.IsVisible = "True";
             }
         }
 
@@ -116,12 +146,25 @@ namespace EventyApp.ViewModel
                 this.ChangeBorderColorPH = "#FF5353";
                 this.Pressed = true;
             }
+            else if (this.changeBorderColorPH == "#FF5353")
+            {
+                this.ChangeBorderColorApartment = "#B7B7B7";
+                this.ChangeBorderColorPH = "#B7B7B7";
+                this.ChangeBorderColorHB = "#B7B7B7";
+                this.Pressed = false;
+                this.IsVisible = "False";
+            }
             else
             {
                 this.ChangeBorderColorPH = "#FF5353";
                 this.ChangeBorderColorApartment = "#B7B7B7";
                 this.ChangeBorderColorHB = "#B7B7B7";
                 this.Pressed = true;
+            }
+
+            if (Pressed)
+            {
+                this.IsVisible = "True";
             }
         }
 
@@ -133,12 +176,25 @@ namespace EventyApp.ViewModel
                 this.ChangeBorderColorHB = "#FF5353";
                 this.Pressed = true;
             }
+            else if (this.changeBorderColorHB == "#FF5353")
+            {
+                this.ChangeBorderColorApartment = "#B7B7B7";
+                this.ChangeBorderColorPH = "#B7B7B7";
+                this.ChangeBorderColorHB = "#B7B7B7";
+                this.Pressed = false;
+                this.IsVisible = "False";
+            }
             else
             {
                 this.ChangeBorderColorHB = "#FF5353";
                 this.ChangeBorderColorApartment = "#B7B7B7";
                 this.ChangeBorderColorPH = "#B7B7B7";
                 this.Pressed = true;
+            }
+
+            if (Pressed)
+            {
+                this.IsVisible = "True";
             }
         }
 
@@ -148,6 +204,7 @@ namespace EventyApp.ViewModel
             this.ChangeBorderColorPH = "#B7B7B7";
             this.ChangeBorderColorHB = "#B7B7B7";
             this.Pressed = false;
+            this.IsVisible = "False";
         }
     }
 }
