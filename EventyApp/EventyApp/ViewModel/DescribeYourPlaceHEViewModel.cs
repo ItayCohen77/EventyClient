ed4a837c689364ffce568316cffefa93158b1bdc
איתי כהן <italcb777@gmail.com>
2022-02-01 23:40:41 +0200

    Added new page.

--- EventyApp/EventyApp/ViewModel/DescribeYourPlaceHEViewModel.cs
+++ EventyApp/EventyApp/ViewModel/DescribeYourPlaceHEViewModel.cs
@@ -22,6 +22,87 @@ namespace EventyApp.ViewModel
 
         private EventyAPIProxy proxy;
 
+        private List<string> apartmentF;
+        public List<string> ApartmentF
+        {
+            get
+            {
+                return this.apartmentF;
+            }
+            set
+            {
+                if (this.apartmentF != value)
+                {
+                    this.apartmentF = value;
+                    OnPropertyChanged(nameof(ApartmentF));
+                }
+            }
+        }
 
+        private List<string> yardF;
+        public List<string> YardF
+        {
+            get
+            {
+                return this.yardF;
+            }
+            set
+            {
+                if (this.yardF != value)
+                {
+                    this.yardF = value;
+                    OnPropertyChanged(nameof(YardF));
+                }
+            }
+        }
+
+        private List<string> hallF;
+        public List<string> HallF
+        {
+            get
+            {
+                return this.hallF;
+            }
+            set
+            {
+                if (this.hallF != value)
+                {
+                    this.hallF = value;
+                    OnPropertyChanged(nameof(HallF));
+                }
+            }
+        }
+
+        private List<string> privateHF;
+        public List<string> PrivateHF
+        {
+            get
+            {
+                return this.privateHF;
+            }
+            set
+            {
+                if (this.privateHF != value)
+                {
+                    this.privateHF = value;
+                    OnPropertyChanged(nameof(PrivateHF));
+                }
+            }
+        }
+
+        public DescribeYourPlaceHEViewModel()
+        {
+            this.ApartmentF.Add("Tv");
+            this.ApartmentF.Add("Air conditioner");
+            this.ApartmentF.Add("Coffee Machine");
+            this.ApartmentF.Add("Water heater");
+            this.ApartmentF.Add("Speaker");
+
+            this.YardF.Add("Pool");
+            this.YardF.Add("BBQ");
+            this.YardF.Add("Table");
+            this.YardF.Add("Chairs");
+            this.YardF.Add("Table");
+        }
     }
 }
